823384f558def7ae9234d460b0c8fe2b1d15ac71
Ivan Max <mixython@gmail.com>
2023-06-11 15:56:05 +0000

    arm_gcc.sh: compilation of new versions (#3)

--- arm_gcc.sh
+++ arm_gcc.sh
@@ -15,6 +15,13 @@ pacman -S elfutils gperf --noconfirm
 		repo_name="arm-linux-gnueabihf-${i}"
 		sudo -Es -H -u ${USER} git clone https://aur.archlinux.org/${repo_name}.git
 		cd ${repo_name}
+		if [ "$i" = "glibc" ]; then
+			sed -i 's/pkgver=2.33/pkgver=2.37/' PKGBUILD
+			sed -i 's/bz27343.patch//' PKGBUILD
+		elif [ "$i" = "gcc" ]; then
+			sed -i 's/pkgver=12.1.0/pkgver=13.1.0/' PKGBUILD
+			sed -i '/844a5c8ca.patch/d' PKGBUILD
+		fi
 		sudo -H -u ${USER} bash -c "makepkg --skipchecksums --skippgpcheck"
 		yes | pacman -U ${repo_name}*
 		cd ..
