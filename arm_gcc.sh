37537ba6f8158f19ddbdbb41793b6080c0882f64
Ivan Max <mixython@gmail.com>
2023-08-09 20:02:14 +0000

    arm_gcc.sh: updating glibc and gcc (#5)

--- arm_gcc.sh
+++ arm_gcc.sh
@@ -16,10 +16,10 @@ pacman -S elfutils gperf --noconfirm
 		sudo -Es -H -u ${USER} git clone https://aur.archlinux.org/${repo_name}.git
 		cd ${repo_name}
 		if [ "$i" = "glibc" ]; then
-			sed -i 's/pkgver=2.33/pkgver=2.37/' PKGBUILD
+			sed -i 's/pkgver=2.33/pkgver=2.38/' PKGBUILD
 			sed -i 's/bz27343.patch//' PKGBUILD
 		elif [ "$i" = "gcc" ]; then
-			sed -i 's/pkgver=12.1.0/pkgver=13.1.0/' PKGBUILD
+			sed -i 's/pkgver=12.1.0/pkgver=13.2.0/' PKGBUILD
 			sed -i '/844a5c8ca.patch/d' PKGBUILD
 		fi
 		sudo -H -u ${USER} bash -c "makepkg --skipchecksums --skippgpcheck"
