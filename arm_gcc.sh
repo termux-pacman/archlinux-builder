ba03748129b053dc44966d3021511c17f67df071
Maxython <mixython@gmail.com>
2023-08-11 00:05:12 +0300

    arm_gcc.sh: gcc compilation fix

--- arm_gcc.sh
+++ arm_gcc.sh
@@ -15,14 +15,10 @@ pacman -S elfutils gperf --noconfirm
 		repo_name="arm-linux-gnueabihf-${i}"
 		sudo -Es -H -u ${USER} git clone https://aur.archlinux.org/${repo_name}.git
 		cd ${repo_name}
-		if [ "$i" = "glibc" ]; then
-			sed -i 's/pkgver=2.33/pkgver=2.38/' PKGBUILD
-			sed -i 's/bz27343.patch//' PKGBUILD
-		elif [ "$i" = "gcc" ]; then
-			sed -i 's/pkgver=12.1.0/pkgver=13.2.0/' PKGBUILD
-			sed -i '/844a5c8ca.patch/d' PKGBUILD
+		if [ "$i" = "gcc" ]; then
+			sed -i 's|https://gmplib.org/download/gmp/|https://ftp.gnu.org/gnu/gmp/|' PKGBUILD
 		fi
-		sudo -H -u ${USER} bash -c "makepkg --skipchecksums --skippgpcheck"
+		sudo -H -u ${USER} bash -c "makepkg --skippgpcheck"
 		yes | pacman -U ${repo_name}*
 		cd ..
 	done
