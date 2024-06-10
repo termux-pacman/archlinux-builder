995092c18f6020c79baf34d4f091af8c41f8d8a5
Maxython <mixython@gmail.com>
2024-06-10 15:39:13 +0300

    global change

--- aarch64_gcc.sh
+++ aarch64_gcc.sh
@@ -2,11 +2,16 @@
 
 set -e
 
+if [ "$(id -u)" = "0" ]; then
+	echo "This script must be run by the user!"
+	exit 1
+fi
+
 # installing gcc-aarch64
-pacman -S aarch64-linux-gnu-gcc --noconfirm
+sudo pacman -Sy aarch64-linux-gnu-gcc --noconfirm
 
 # installing sdt.h and sdt-config.h
-curl https://gitlab.archlinux.org/archlinux/packaging/packages/glibc/-/raw/main/sdt.h \
+sudo curl https://gitlab.archlinux.org/archlinux/packaging/packages/glibc/-/raw/main/sdt.h \
 	-o /usr/aarch64-linux-gnu/include/sys/sdt.h
-curl https://gitlab.archlinux.org/archlinux/packaging/packages/glibc/-/raw/main/sdt-config.h \
+sudo curl https://gitlab.archlinux.org/archlinux/packaging/packages/glibc/-/raw/main/sdt-config.h \
 	-o /usr/aarch64-linux-gnu/include/sys/sdt-config.h
